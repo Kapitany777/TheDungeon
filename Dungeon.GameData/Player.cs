73f5e077f00400e30715aa8354a110b839b598d8
Viktor Török <torokv@vnet.hu>
2021-01-21 21:30:43 +0100

    Changing .NET version to 4.8

--- Dungeon.GameData/Player.cs
+++ Dungeon.GameData/Player.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dungeon.Names
+namespace Dungeon.GameData
 {
-    public class Class1
+    public class Player : BaseData
     {
+        public Player()
+        {
+
+        }
     }
 }
